0841cbf3ff916df0a6113bdc6e97b618bdaae88a
Lauri Kuresoo <lauri.kuresoo@ut.ee>
2024-03-24 16:45:39 +0200

    Final commit

---
+++ src/test/java/com/example/fooddelivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void testCalculator(){
+
+
+	}
+
+}
